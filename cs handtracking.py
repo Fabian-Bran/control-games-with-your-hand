9aeed4c11dd9afd7c5927586432179afda44386d
Fabian <branandreifabian2020@gmail.com>
2024-08-17 00:17:02 +0100

    Update cs handtracking.py
    added the ability to shoot

--- cs handtracking.py
+++ cs handtracking.py
@@ -11,17 +11,32 @@ mphands=mp.solutions.hands
 #change these based on preference:
 distance=70 # palm distance away from center requiered in order to begin
 sensitivity=5 #higher number = slower
+trigger_sensitivity=5 #how close your landmarks need to be to trigger the action
 
 mouse_x, mouse_y = pyautogui.position() 
 
 capture=cv2.VideoCapture(0)
 hands=mphands.Hands()
 
-def findDistanceFromTarget(x,y,target):
-   x_distance = x - target[0]
-   y_distance = y - target[1]
+#used for the mouse
+def findDistanceFromCenter(x,y,target):
 
-   return (x_distance, y_distance)
+    x_distance = x - target[0]
+    y_distance = y - target[1]
+    
+
+    return (x_distance, y_distance)
+
+      
+#used for gesture controls
+def findDistanceFromTarget(x,y,target):
+    x_distance = x - target.x
+    y_distance = y - target.y
+    x_distance = abs(x_distance * 100)
+    y_distance = abs(y_distance * 100)
+    return (x_distance, y_distance)
+
+   
 
 while True:
     #image is processed and some variables are assigned
@@ -43,19 +58,23 @@ while True:
         #the index refers to a keypoint on the hand (refer to the image included if you want to add something)
         wrist = hand_landmarks.landmark[0] 
         middle_finger_mcp = hand_landmarks.landmark[9]
+        thumb_tip = hand_landmarks.landmark[4]
+        index_finger_tip = hand_landmarks.landmark[8]
+
         
         #using the position of the wrist and the bottom of the middle finger to aproximate the palms location 
         palm_x = ((wrist.x + middle_finger_mcp.x)/2)* img_w
         palm_y = ((middle_finger_mcp.y + wrist.y) /2)* img_h
 
-        palm_distance = findDistanceFromTarget(palm_x,palm_y,midpoint)
+        palm_distance = findDistanceFromCenter(palm_x,palm_y,midpoint)
+        thumb_index_tip_distance = findDistanceFromTarget(thumb_tip.x,thumb_tip.y,index_finger_tip)
 
-        #if palm outside box
-        if palm_distance[0] > distance or palm_distance[0] < -abs(distance) or palm_distance[1] > distance or palm_distance[1] < -abs(distance):
+        if thumb_index_tip_distance[0] < trigger_sensitivity and thumb_index_tip_distance[1] < trigger_sensitivity:
+            pyautogui.click()
+        elif palm_distance[0] > distance or palm_distance[0] < -abs(distance) or palm_distance[1] > distance or palm_distance[1] < -abs(distance):
             mouse_x += palm_distance[0] / sensitivity
             mouse_y += palm_distance[1] / sensitivity
             pydirectinput.moveTo(int(mouse_x),int(mouse_y),relative=True,_pause=False)
-        #if palm inside box
         else:
             pass
             
